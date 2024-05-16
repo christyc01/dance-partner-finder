57904c0de02e9310b3a7e82561aba4658ae306d1
Christy C <57055910+christyc01@users.noreply.github.com>
2024-05-15 23:48:41 -0600

    Make create form prettier

--- frontend/src/pages/CreateDancer.jsx
+++ frontend/src/pages/CreateDancer.jsx
@@ -77,62 +77,86 @@ const CreateDancer = () => {
   };
 
   return (
-    <div className="p-4">
+    <div className="p-4 bg-white shadow-lg rounded-lg">
       <BackButton />
       {loading ? <Spinner /> : ''}
-      <h1 className="text-3xl my-4">Create Dancer</h1>
-      <form
-        onSubmit={createDancer}
-        className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto"
-      >
-        <div className="my-4">
-          <label htmlFor="name" className="text-xl mr-4 text-gray-500">
-            Name:
-          </label>
-          <input
-            id="name"
-            placeholder="Enter name"
-            type="text"
-            value={formValues.name}
-            onChange={handleChange}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
-          />
-        </div>
-        <div className="my-4">
-          <label htmlFor="location" className="text-xl mr-4 text-gray-500">
-            Location:
-          </label>
-          <input
-            id="location"
-            placeholder="Enter location"
-            type="text"
-            value={formValues.location}
-            onChange={handleChange}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
-          />
-        </div>
+      <div className="flex  flex-col w-full items-center">
+        <div className="w-96">
+          <h1 className="text-xl font-semibold mb-2 text-green-600">
+            Create Dancer
+          </h1>
+          <form
+            onSubmit={createDancer}
+            className="flex flex-col border-2  bg-green-200 border-green-300 rounded-md p-6"
+          >
+            <div className="my-4">
+              <label
+                htmlFor="name"
+                className="text-xl mr-4 text-green-600 font-bold"
+              >
+                Name:
+              </label>
+              <input
+                id="name"
+                placeholder="Enter name"
+                type="text"
+                value={formValues.name}
+                onChange={handleChange}
+                className="border-2 border-green-200 px-4 py-2 w-full"
+              />
+            </div>
+            <div className="my-4">
+              <label
+                htmlFor="location"
+                className="text-xl mr-4 text-green-600 font-bold"
+              >
+                Location:
+              </label>
+              <input
+                id="location"
+                placeholder="Enter location"
+                type="text"
+                value={formValues.location}
+                onChange={handleChange}
+                className="border-2 border-green-200 px-4 py-2 w-full"
+              />
+            </div>
 
-        {danceStyleOptions.map((danceStyleOption) => (
-          <div key={danceStyleOption}>
-            <input
-              id={danceStyleOption}
-              type="checkbox"
-              value={danceStyleOption}
-              onChange={handleCheckboxChange}
-            />
-            <label
-              htmlFor={danceStyleOption}
-              className="text-xl mr-4 text-gray-500"
+            <div className="my-4">
+              <label
+                htmlFor="location"
+                className="text-xl mr-4 text-green-600 font-bold"
+              >
+                Select dance styles:
+              </label>
+              {danceStyleOptions.map((danceStyleOption) => (
+                <div key={danceStyleOption} className="my-6">
+                  <input
+                    id={danceStyleOption}
+                    type="checkbox"
+                    value={danceStyleOption}
+                    onChange={handleCheckboxChange}
+                    className="hidden peer/draft"
+                  />
+                  <label
+                    htmlFor={danceStyleOption}
+                    className="text-xl mr-4 py-2 px-4 rounded-full transition-colors border-2 border-green-500 text-green-600 peer-checked/draft:bg-green-500 peer-checked/draft:text-white"
+                  >
+                    {danceStyleOption.slice(0, 1).toUpperCase()}
+                    {danceStyleOption.slice(1)}
+                  </label>
+                </div>
+              ))}
+            </div>
+            <button
+              type="submit"
+              className="p-2 bg-green-500 text-white m-8 rounded-full transition-colors"
             >
-              {danceStyleOption.slice(0, 1).toUpperCase()}
-              {danceStyleOption.slice(1)}
-            </label>
-          </div>
-        ))}
-        <button type="submit" className="p-2 bg-sky-300 m-8">
-          Submit
-        </button>
-      </form>
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
