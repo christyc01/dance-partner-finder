face3c255d1ce4cfcb6c36027925045185f08377
Christy C <57055910+christyc01@users.noreply.github.com>
2024-05-17 13:53:27 -0600

    Move filters into their own component

---
+++ frontend/src/components/Filters.jsx
@@ -0,0 +1,95 @@
+/* eslint-disable react/prop-types */
+const Filters = ({
+  locationsToShow,
+  setLocationsToShow,
+  danceStylesToShow,
+  setDanceStylesToShow,
+  allUniqueLocationsArray,
+  allUniqueDanceStylesArray,
+}) => {
+  return (
+    <div className="flex flex-col lg:flex-row justify-between mb-6">
+      <div className="bg-white shadow-lg p-4 md:rounded-lg w-full lg:w-1/2 mr-4">
+        <h3 className="text-xl font-semibold mb-2 text-emerald-600">
+          Filter by Location
+        </h3>
+        <div className="flex flex-wrap gap-2">
+          <button
+            onClick={() => setLocationsToShow('')}
+            className={`py-2 px-4 rounded-full transition-colors ${
+              !locationsToShow.length
+                ? 'bg-emerald-500 text-white'
+                : 'bg-emerald-200 text-emerald-700'
+            }`}
+          >
+            View all
+          </button>
+          {allUniqueLocationsArray.map((uniqueLocation) => (
+            <button
+              key={uniqueLocation}
+              onClick={() =>
+                locationsToShow.includes(uniqueLocation)
+                  ? setLocationsToShow(
+                      locationsToShow.filter(
+                        (item) => !(item === uniqueLocation)
+                      )
+                    )
+                  : setLocationsToShow([...locationsToShow, uniqueLocation])
+              }
+              className={`py-2 px-4 rounded-full transition-colors ${
+                locationsToShow.includes(uniqueLocation)
+                  ? 'bg-emerald-500 text-white'
+                  : 'bg-emerald-200 text-emerald-700'
+              }`}
+            >
+              {uniqueLocation}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="bg-white shadow-lg p-4 md:rounded-lg w-full lg:w-1/2">
+        <h3 className="text-xl font-semibold mb-2 text-emerald-600">
+          Filter by Dance Style
+        </h3>
+        <div className="flex flex-wrap gap-2">
+          <button
+            onClick={() => setDanceStylesToShow([])}
+            className={`py-2 px-4 rounded-full transition-colors ${
+              !danceStylesToShow.length
+                ? 'bg-emerald-500 text-white'
+                : 'bg-emerald-200 text-emerald-700'
+            }`}
+          >
+            View all
+          </button>
+          {allUniqueDanceStylesArray.map((uniqueDanceStyle) => (
+            <button
+              key={uniqueDanceStyle}
+              onClick={() =>
+                danceStylesToShow.includes(uniqueDanceStyle)
+                  ? setDanceStylesToShow(
+                      danceStylesToShow.filter(
+                        (item) => !(item === uniqueDanceStyle)
+                      )
+                    )
+                  : setDanceStylesToShow([
+                      ...danceStylesToShow,
+                      uniqueDanceStyle,
+                    ])
+              }
+              className={`py-2 px-4 rounded-full transition-colors ${
+                danceStylesToShow.includes(uniqueDanceStyle)
+                  ? 'bg-emerald-500 text-white'
+                  : 'bg-emerald-200 text-emerald-700'
+              }`}
+            >
+              {uniqueDanceStyle}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Filters;
--- frontend/src/components/home/DancersTable.jsx
+++ frontend/src/components/home/DancersTable.jsx
@@ -4,6 +4,7 @@ import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineDelete } from 'react-icons/md';
 import { Link } from 'react-router-dom';
+import Filters from '../Filters';
 
 const DancersTable = ({ data, showOperations }) => {
   const [dataToShow, setDataToShow] = useState(data);
@@ -52,87 +53,14 @@ const DancersTable = ({ data, showOperations }) => {
 
   return (
     <div className="p-0 md:p-4">
-      <div className="flex flex-col lg:flex-row justify-between mb-6">
-        <div className="bg-white shadow-lg p-4 md:rounded-lg w-full lg:w-1/2 mr-4">
-          <h3 className="text-xl font-semibold mb-2 text-emerald-600">
-            Filter by Location
-          </h3>
-          <div className="flex flex-wrap gap-2">
-            <button
-              onClick={() => setLocationsToShow('')}
-              className={`py-2 px-4 rounded-full transition-colors ${
-                !locationsToShow.length
-                  ? 'bg-emerald-500 text-white'
-                  : 'bg-emerald-200 text-emerald-700'
-              }`}
-            >
-              View all
-            </button>
-            {allUniqueLocationsArray.map((uniqueLocation) => (
-              <button
-                key={uniqueLocation}
-                onClick={() =>
-                  locationsToShow.includes(uniqueLocation)
-                    ? setLocationsToShow(
-                        locationsToShow.filter(
-                          (item) => !(item === uniqueLocation)
-                        )
-                      )
-                    : setLocationsToShow([...locationsToShow, uniqueLocation])
-                }
-                className={`py-2 px-4 rounded-full transition-colors ${
-                  locationsToShow.includes(uniqueLocation)
-                    ? 'bg-emerald-500 text-white'
-                    : 'bg-emerald-200 text-emerald-700'
-                }`}
-              >
-                {uniqueLocation}
-              </button>
-            ))}
-          </div>
-        </div>
-        <div className="bg-white shadow-lg p-4 md:rounded-lg w-full lg:w-1/2">
-          <h3 className="text-xl font-semibold mb-2 text-emerald-600">
-            Filter by Dance Style
-          </h3>
-          <div className="flex flex-wrap gap-2">
-            <button
-              onClick={() => setDanceStylesToShow([])}
-              className={`py-2 px-4 rounded-full transition-colors ${
-                !danceStylesToShow.length
-                  ? 'bg-emerald-500 text-white'
-                  : 'bg-emerald-200 text-emerald-700'
-              }`}
-            >
-              View all
-            </button>
-            {allUniqueDanceStylesArray.map((uniqueDanceStyle) => (
-              <button
-                key={uniqueDanceStyle}
-                onClick={() =>
-                  danceStylesToShow.includes(uniqueDanceStyle)
-                    ? setDanceStylesToShow(
-                        danceStylesToShow.filter(
-                          (item) => !(item === uniqueDanceStyle)
-                        )
-                      )
-                    : setDanceStylesToShow([
-                        ...danceStylesToShow,
-                        uniqueDanceStyle,
-                      ])
-                }
-                className={`py-2 px-4 rounded-full transition-colors ${
-                  danceStylesToShow.includes(uniqueDanceStyle)
-                    ? 'bg-emerald-500 text-white'
-                    : 'bg-emerald-200 text-emerald-700'
-                }`}
-              >
-                {uniqueDanceStyle}
-              </button>
-            ))}
-          </div>
-        </div>
-      </div>
+      <Filters
+        locationsToShow={locationsToShow}
+        setLocationsToShow={setLocationsToShow}
+        danceStylesToShow={danceStylesToShow}
+        setDanceStylesToShow={setDanceStylesToShow}
+        allUniqueLocationsArray={allUniqueLocationsArray}
+        allUniqueDanceStylesArray={allUniqueDanceStylesArray}
+      />
 
       {/* SMALL SCREENS */}
       <table className="md:hidden w-full border-separate table-fixed">
