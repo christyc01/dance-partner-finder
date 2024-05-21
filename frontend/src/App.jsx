ba3c6d4cd25cbaf6b1b19c70f7f46bd332559bb9
Christy C <57055910+christyc01@users.noreply.github.com>
2024-05-21 11:42:15 -0600

    Add dance events route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import DeleteDancer from './pages/DeleteDancer';
 import EditDancer from './pages/EditDancer';
 import Home from './pages/Home';
 import ShowDancer from './pages/ShowDancer';
+import ShowDanceEvent from './pages/ShowDanceEvent';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/dancers/delete/:id" element={<DeleteDancer />} />
       <Route path="/dancers/edit/:id" element={<EditDancer />} />
       <Route path="/dancers/details/:id" element={<ShowDancer />} />
+      <Route path="/dance-events" element={<ShowDanceEvent />} />
     </Routes>
   );
 };
