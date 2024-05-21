1e1582c372feaf686085a87d95053e0c5af4d559
Christy C <57055910+christyc01@users.noreply.github.com>
2024-05-21 11:20:23 -0600

    Add validation, tidy up comments

--- backend/routes/dance-events/danceEventsRoute.js
+++ backend/routes/dance-events/danceEventsRoute.js
@@ -5,16 +5,15 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
   try {
-    // validation - check if any of the required fields are missing - if so, return a message to send required fields #TODO
-    // define a new danceEvent object with all the available fields to be received from the client, to be saved (gotten from the body of the request)
+    if (!req.body.eventName || !req.body.location) {
+      return res.status(500).json({ message: 'Missing a required field' });
+    }
     const danceEvent = {
       eventName: req.body.eventName,
       location: req.body.location,
       danceStyles: req.body.danceStyles,
     };
-    // define a new variable that uses the model to create a new danceEvent object
     const newDanceEvent = await DanceEventModel.create(danceEvent);
-    // return & send it
     return res.status(201).send(newDanceEvent);
   } catch (error) {
     console.log(error);
@@ -34,48 +33,36 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Get a specific dance event by id
 router.get('/:id', async (req, res) => {
   try {
-    // const danceEvent = await DanceEventModel.find({ _id: req.params.id }); // returns 'data' inside an object, and directly inside that is an array with an object inside that
-    const danceEvent = await DanceEventModel.findById(req.params.id); // returns 'data' inside an object, with an object inside of that
-    // return res.status(200).json({ data: danceEvent }); // returns {data: {...}}
-    return res.status(200).json(danceEvent); // less cluttered
+    const danceEvent = await DanceEventModel.findById(req.params.id);
+    return res.status(200).json(danceEvent);
   } catch (error) {
     console.log(error.message);
     res.status(500).send({ message: error.message });
   }
 });
 
-// Update a specific dance event
 router.put('/:id', async (req, res) => {
   try {
-    // add validation
-    console.log(req.params);
-    console.log(req.body);
     const danceEventToUpdate = await DanceEventModel.findByIdAndUpdate(
       req.params.id,
-      // can just pass in the body in general
-      // { location: req.body.location },
-      // {
-      //   new: true,
-      // }
-      { eventName: 'updated in the code - NEW', location: req.body.location },
+      req.body,
       {
         new: true,
       }
     );
-    // If there's no danceEventToUpdate: return res.status(404).json({ message: 'Dance event not found' });
-    // Otherwise return message that dance event was updated
-    console.log(req.params.id);
-    return res.status(200).json(danceEventToUpdate);
+    if (!danceEventToUpdate) {
+      return res.status(404).json({ message: 'Dance event not found' });
+    }
+    return res
+      .status(200)
+      .json({ message: 'Dance event updated', data: danceEventToUpdate });
   } catch (error) {
     console.log(error.message);
     res.status(500).send({ message: error.message });
   }
 });
-
-// Delete a dance event
 
 router.delete('/:id', async (req, res) => {
   try {
