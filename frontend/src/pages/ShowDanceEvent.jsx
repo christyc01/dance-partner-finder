01148fa9f1ca98b7d9b8dd5fb7bef6ccb69e5f9e
Christy C <57055910+christyc01@users.noreply.github.com>
2024-05-21 22:56:49 -0600

    Add more details and styling to dance events home page

--- frontend/src/pages/ShowDanceEvent.jsx
+++ frontend/src/pages/ShowDanceEvent.jsx
@@ -1,16 +1,24 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
 
 const ShowDanceEvent = () => {
   const [danceEventData, setDanceEventData] = useState({});
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const getDanceEventData = () => {
+      setLoading(true);
       axios
         .get('http://localhost:5555/dance-events')
-        .then((response) => setDanceEventData(response))
+        .then((response) => {
+          setDanceEventData(response.data);
+          setLoading(false);
+        })
         .catch((error) => {
           console.log(error);
+          setLoading(false);
         });
     };
     getDanceEventData();
@@ -19,9 +27,34 @@ const ShowDanceEvent = () => {
   console.log('eventData:', danceEventData);
 
   return (
-    <div>
-      <h1>ShowDanceEvent</h1>
-      <p>{danceEventData.data.data[0].eventName}</p>
+    <div className="md:p-4 bg-white shadow-lg md:rounded-lg">
+      <BackButton />
+      <div className="flex flex-col w-full items-center">
+        <div className="w-full xl:w-3/4">
+          <h1 className="text-2xl font-bold mb-2 text-emerald-600">
+            Show Dance Event
+          </h1>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <div className="p-0 md:p-4 mt-2 grid lg:grid-cols-3 xl:grid-cols-4 justify-center">
+              {danceEventData.data &&
+                danceEventData.data.map((event) => (
+                  <div
+                    key={event._id}
+                    className="border-2 border-emerald-500 m-4 p-4 rounded-lg bg-emerald-50"
+                  >
+                    <p className="font-bold">{event.eventName}</p>
+                    <ul className="list-disc pl-8">
+                      <li>{event.location}</li>
+                      <li>{event.danceStyles}</li>
+                    </ul>
+                  </div>
+                ))}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
