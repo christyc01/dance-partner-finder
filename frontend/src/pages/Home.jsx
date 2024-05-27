e27ccbb3557e2137b2b228a12ba5d72ef4b60606
Christy C <57055910+christyc01@users.noreply.github.com>
2024-05-27 17:11:35 -0600

    Prettier buttons

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,10 +3,16 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="flex justify-center h-full items-center">
-      <Link to="/dancers" className="bg-orange-500 p-4 rounded-l-full">
+      <Link
+        to="/dancers"
+        className="border-4 border-r-2 border-emerald-700 bg-emerald-50 hover:bg-emerald-500 hover:text-white p-4 rounded-l-full text-3xl text-nowrap"
+      >
         Dancers
       </Link>
-      <Link to="/dance-events" className="bg-yellow-500 p-4 rounded-r-full">
+      <Link
+        to="/dance-events"
+        className="border-4 border-l-2 border-emerald-700 bg-emerald-50 hover:bg-emerald-500 hover:text-white p-4 rounded-r-full text-3xl text-nowrap"
+      >
         Dance Events
       </Link>
     </div>
