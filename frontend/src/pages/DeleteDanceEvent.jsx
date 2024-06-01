52ab03103253162c014982049748c795f5dbd73e
Christy C <57055910+christyc01@users.noreply.github.com>
2024-06-01 10:51:15 -0600

    Delete dance event function working

--- frontend/src/pages/DeleteDanceEvent.jsx
+++ frontend/src/pages/DeleteDanceEvent.jsx
@@ -1,5 +1,26 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+
 const DeleteDanceEvent = () => {
-  return <h1>DeleteDanceEvent</h1>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const handleDelete = () => {
+    console.log('deleting...', id);
+    axios
+      .delete(`http://localhost:5555/dance-events/${id}`)
+      .then(navigate('/'))
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  return (
+    <div>
+      <h1>DeleteDanceEvent</h1>
+      <button onClick={handleDelete}>Delete me</button>
+    </div>
+  );
 };
 
 export default DeleteDanceEvent;
