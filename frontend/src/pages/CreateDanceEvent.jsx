728d72d1c31b40aeff4834e6143a352ba5925206
Christy C <57055910+christyc01@users.noreply.github.com>
2024-06-01 11:55:42 -0600

    Add multiple fields and grab the input from each

--- frontend/src/pages/CreateDanceEvent.jsx
+++ frontend/src/pages/CreateDanceEvent.jsx
@@ -4,16 +4,42 @@ const CreateDanceEvent = () => {
   const [formValues, setFormValues] = useState({});
 
   const handleChange = (e) => {
-    setFormValues({ test: e.target.value });
+    setFormValues({ ...formValues, [e.target.id]: e.target.value });
   };
 
   console.log('formValues:', formValues);
 
   return (
     <div>
-      <h1>CreateDanceEvent</h1>
-      <form action="submit">
+      <h1 className="font-bold py-4">CreateDanceEvent</h1>
+      <form action="submit" className="flex flex-col">
+        <label htmlFor="eventName">Event name:</label>
         <input
+          id="eventName"
+          type="text"
+          placeholder="Holding the place"
+          className="bg-emerald-50 p-4 rounded-xl"
+          onChange={(e) => handleChange(e)}
+        />
+        <label htmlFor="location">Location:</label>
+        <input
+          id="location"
+          type="text"
+          placeholder="Holding the place"
+          className="bg-emerald-50 p-4 rounded-xl"
+          onChange={(e) => handleChange(e)}
+        />
+        <label htmlFor="danceStyles">DanceStyles:</label>
+        <input
+          id="danceStyles"
+          type="text"
+          placeholder="Holding the place"
+          className="bg-emerald-50 p-4 rounded-xl"
+          onChange={(e) => handleChange(e)}
+        />
+        <label htmlFor="attendees">Attendees:</label>
+        <input
+          id="attendees"
           type="text"
           placeholder="Holding the place"
           className="bg-emerald-50 p-4 rounded-xl"
