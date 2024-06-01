b91282dabf22625cf53b4d0e9601541e7b5948d6
Christy C <57055910+christyc01@users.noreply.github.com>
2024-06-01 11:05:32 -0600

    Add spinner, back button, loading state, and navigation after deletion

--- frontend/src/pages/DeleteDanceEvent.jsx
+++ frontend/src/pages/DeleteDanceEvent.jsx
@@ -1,22 +1,32 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
+import { useState } from 'react';
+import Spinner from '../components/Spinner';
+import BackButton from '../components/BackButton';
 
 const DeleteDanceEvent = () => {
   const { id } = useParams();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const handleDelete = () => {
-    console.log('deleting...', id);
+    setLoading(true);
     axios
       .delete(`http://localhost:5555/dance-events/${id}`)
-      .then(navigate('/'))
+      .then(() => {
+        navigate('/');
+        setLoading(false);
+      })
       .catch((error) => {
         console.log(error.message);
+        setLoading(false);
       });
   };
 
   return (
     <div>
+      <BackButton />
+      {loading ? <Spinner /> : ''}
       <h1>DeleteDanceEvent</h1>
       <button onClick={handleDelete}>Delete me</button>
     </div>
